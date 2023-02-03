ee72b0b8eda18a28e730dd6d9ca0f8b5b2d00f8b
Philip McElmurray <philmcelmurray@gmail.com>
2023-02-03 12:53:37 -0500

    Travaillé sur lapply et sapply.

---
+++ lapply_et_sapply/customTests.R
@@ -0,0 +1,17 @@
+calculates_same_value <- function(expr){
+  e <- get("e", parent.frame())
+  # Calculate what the user should have done.
+  eSnap <- cleanEnv(e$snapshot)
+  val <- eval(parse(text=expr), eSnap)
+  passed <- isTRUE(all.equal(val, e$val))
+  if(!passed)e$delta <- list()
+  return(passed)
+}
+
+getState <- function(){
+  environment(sys.function(1))$e
+}
+
+getVal <- function(){
+  getState()$val
+}
---
+++ lapply_et_sapply/initLesson.R
@@ -0,0 +1,26 @@
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+.lessonpath <- file.path(.get_course_path(), "Programmation_R",
+                        "lapply_and_sapply")
+
+.datapath <- file.path(.lessonpath, "donnees_des_drapeaux.txt")
+
+drapeaux <- read.csv(.datapath, header=FALSE)
+
+colnames(drapeaux) <- c("nom", "masse_continentale", "zone", "region", "population",
+                     "langue", "religion", "barres", "bandes", "couleurs",
+                     "rouge", "vert", "bleu", "or", "blanc", "noir",
+                     "orange", "teinte_principale", "cercles", "croix", "saltires",
+                     "quartiers", "etoiles_solaires", "croissant", "triangle",
+                     "symbole", "animer", "texte", "haut_gauche", "bas_droite")
+
+.infopath <- file.path(.lessonpath, "noms_des_drapeaux.txt")
+
+afficher_les_informations <- function() {
+  file.edit(.infopath)
+  return(.infopath)
+}
